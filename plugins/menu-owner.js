141d81987f664795ae3029993bb75a1df68da730
AL3ABDO <indopro476@gmail.com>
2024-05-16 17:40:05 +0100

    Update menu-owner.js

--- plugins/menu-owner.js
+++ plugins/menu-owner.js
@@ -9,14 +9,14 @@ import fetch from 'node-fetch'
 const defaultMenu = {
   before: `
 ╾━━━━━━━━━━━━━━━━━╼
-⌬ *SALAM 😆:*  %name 
+⌬ *سلام عليكم 👋:*  %name 
 ╾━━━━━━━━━━━━━━━━━╼
 %readmore
 `.trimStart(),
   header: '╭─────≼ %category ≽',
   body: '╎≡ %cmd',
   footer: '╰┄┄┄┄┄┄┄┄┄┄┄┄┄〢',
-  after: 'BOBIZA 👑',
+  after: 'A L 3 A M E B O T ❤️',
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
@@ -179,8 +179,8 @@ let tags = {
 	externalAdReply: {
 	title: 'قسم خاص بصاحب البوت فقط',
 	body: 'اليك الاوامر الخاصة بهذا القسم',
-	thumbnailUrl: 'https://telegra.ph/file/22455a32e324068fa1922.jpg',
-	sourceUrl: 'instagram.com/noureddine_ouafy',
+	thumbnailUrl: 'https://telegra.ph/file/cc6c949df04097d566fad.jpg',
+	sourceUrl: 'https://t.me/AL3AMEABDO',
 	mediaType: 1,
 	renderLargerThumbnail: true
 	}}})
